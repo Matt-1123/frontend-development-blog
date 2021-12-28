cf4c3cba5d982d81328354900a573f600c101b6b
Matt Russo <matthew.russo91@gmail.com>
2021-12-27 21:03:36 -0500

    add single post template with comments template

---
+++ single.php
@@ -0,0 +1,20 @@
+<?php
+	get_header();
+?>
+
+	<article class="content px-3 py-5 p-md-5">
+		<?php 
+			if( have_posts() ) {
+				while(have_posts()){
+					the_post();
+					
+          // parameters: file path, type
+          get_template_part('template-parts/content', 'article');
+				}
+			}
+		?>
+	</article>
+	
+<?php
+	get_footer();
+?>
